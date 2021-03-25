9855b6101c30a308905e0c07ad5e2488a9d577f0
Lubov May <liubov.mayorova@gmail.com>
2021-03-25 16:02:25 -0700

    Adding avatar, name, handle and button

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -6,8 +6,16 @@ class ListContacts extends Component {
         return (
             <ol className="contact-list" >
                 {this.props.contacts.map(person =>
-                    <li key={person.id}>
-                        {person.name}
+                    <li key={person.id} className="contact-list-item">
+                        <div
+                            className="contact-avatar"
+                            style={{ backgroundImage: `url(${person.avatarURL})` }}>
+                        </div>
+                        <div className="contact-details">
+                            <p>{person.name}</p>
+                            <p>@{person.handle}</p>
+                        </div>
+                        <button className="contact-remove">Remove</button>
                     </li>)}
             </ol>
         )
